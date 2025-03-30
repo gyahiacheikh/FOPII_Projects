b52bb05c8b9e52cc6a877c70d30c950b9c6cfd8d
saraadiiez <sdiezgras@gmail.com>
2025-03-30 15:01:51 +0200

    domingo15

--- s_version.c
+++ s_version.c
@@ -8,25 +8,39 @@ Simulator - main program
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "project1.h" // header with all data structures
 
+int booksRemoved = 0;
+int platesRemoved = 0;
+int robotsRemoved = 0;
+
 //----------------------------------------------------------General
 // WARNING: do not change this function
-enum EventType GenerateEventType() {
+enum EventType GenerateEventType()
+{
 	return rand()%3;
 }
 
-void CheckArguments (int argc, char ** argv){
-	if (argc != 2){
+void CheckArguments (int argc, char ** argv)
+{
+	if (argc != 2) {
         printf("The number of arguments must be two");
-        exit(1);
+		exit(1);
     }
+
+	int EventNumbers = atoi(argv[1]); // Convert the argument to an integer
+    if (EventNumbers <= 0) {
+        printf("The number of events must be a positive integer.\n");
+        exit(1); 
+    };
 }
 
 //----------------------------------------------------------RobotPackages -> Sorted list
 // WARNING: do not change this function
-struct RobotPackage * GenerateRobotPackage() {
+struct RobotPackage * GenerateRobotPackage()
+{
 	// reserve memory for a RobotPackage
 	struct RobotPackage * RobotPackage=malloc(sizeof(struct RobotPackage));
 	int RobotPackageNum=rand()%8;
@@ -38,27 +52,70 @@ struct RobotPackage * GenerateRobotPackage() {
 	return RobotPackage;
 }
 
+struct RobotPackage* robotPackageList = NULL;
 // function to print a list of RobotPackages
-void PrintRobotPackages() {
-	
+void PrintRobotPackages(struct RobotPackage* head) 
+{
+    struct RobotPackage* temp = head;
+
+    if (temp == NULL) {
+        printf("No Robot Packages available.\n");
+        return;
+    }
+
+    printf("Robot Packages (Sorted List):\n");
+    while (temp != NULL) {
+        printf("Supplier: %s, ID: %s, Year: %d\n", temp->supplier, temp->id, temp->year);
+        temp = temp->next;
+    }
+    printf("\n");
 }
 
 // function to search for a RobotPackage
-struct RobotPackage * SearchRobotPackage(/*...*/)
-{
-
+struct RobotPackage* SearchRobotPackage(struct RobotPackage* head, char* supplier) {
+    struct RobotPackage* temp = head;
+    while (temp != NULL) {
+        if (strcmp(temp->supplier, supplier) == 0) {
+            return temp; // Found the package
+        }
+        temp = temp->next;
+    }
+    return NULL; // Not found
 }
 
 // function to simulate an insertion of RobotPackages in a ordered way (sorted by supplier)
 void SimulateManagingRobotPackages(struct RobotPackage * RobotPackage)
 {
+	if (robotPackageList == NULL || strcmp(RobotPackage->supplier, robotPackageList->supplier) < 0) {
+        // Insert at the beginning
+        RobotPackage->next = robotPackageList;
+        robotPackageList = RobotPackage;
+        return;
+    }
 
+    // Traverse the list to find the correct position
+    struct RobotPackage *current = robotPackageList;
+    while (current->next != NULL && strcmp(current->next->supplier, RobotPackage->supplier) < 0) {
+        current = current->next;
+    }
+
+    // Insert the package in the correct position
+    RobotPackage->next = current->next;
+    current->next = RobotPackage;
 }
 
 // function to remove all the RobotPackages from the list at the end of the program
 void RemoveAllRobotPackages()
 {
+	struct RobotPackage* current = robotPackageList;
+    struct RobotPackage* nextNode;
 
+    while (current != NULL) {
+        nextNode = current->next;
+        free(current);
+        current = nextNode;
+    }
+    robotPackageList = NULL;
 }
 
 //----------------------------------------------------------Packages -> different Stacks
@@ -76,27 +133,65 @@ struct Package * GeneratePackage()
 }
 
 // function to initialize all stacks of Packages 
-void InitStacks()
-{
+struct Package** stacks;
+int* top;
 
+void InitStacks() 
+{
+	stacks = (struct Package**)malloc(3 * sizeof(struct Package*)); // Allocate space for 3 stacks
+	top = (int*)malloc(3 * sizeof(int)); // Allocate space for top indices
+
+	if (!stacks || !top) {
+		printf("Memory allocation failed for stacks.\n");
+		exit(1);
+	}
+
+	for (int i = 0; i < 3; i++) {
+		stacks[i] = (struct Package*)malloc(MAX_CAPACITY * sizeof(struct Package));
+		if (!stacks[i]) {
+			printf("Memory allocation failed for stack %d.\n", i);
+			exit(1);
+		}
+		top[i] = -1; // Initialize as empty
+	}
 }
 
 // function to print all stacks with all Packages
 void PrintPackages()
 {
-
+	for (int i = 0; i < 3; i++) {
+		printf("Stack %d:\n", i);
+		for (int j = 0; j <= top[i]; j++) {
+			printf("Package Type: %d, Color: %d\n", stacks[i][j].type, stacks[i][j].color);
+		}
+		printf("\n");
+	}
 }
 
 // function to remove all packages from a given stack when its MAX_CAPACITY is reached
-void RemoveStack(/*...*/)
-{
-
+void RemoveStack(int stackIndex, struct Package **stacks, int *top) {
+	if (stackIndex == 0) { // Books are in stack 0
+        booksRemoved += (top[stackIndex] + 1);
+    } else if (stackIndex == 1) { // Plates are in stack 1
+        platesRemoved += (top[stackIndex] + 1);
+    } else if (stackIndex == 2) { // Robots are in stack 2
+		robotsRemoved += (top[stackIndex] + 1);
+	}
+	
+    top[stackIndex] = -1; // Reset stack
 }
 
 // function to simulate putting a generated Package to a corresponding stack depending on the type (size)
 void SimulateClassifyPackage(struct Package * Package)
 {
+	int stackIndex = Package->type;
 
+	if (top[stackIndex] >= MAX_CAPACITY - 1) {
+		RemoveStack(stackIndex, stacks, top);
+	}
+
+	top[stackIndex]++;
+	stacks[stackIndex][top[stackIndex]] = *Package;
 }
 
 // function to clean all stacks before the end of the program
@@ -122,7 +217,15 @@ struct Shopping * GenerateShopping()
 // function to print a list of robots in a shopping queue
 void PrintShopping()
 {
+	printf("STATISTICS WHEN CLEANING THE SIMULATION:\n");
+    printf("Removing books...\n");
+    printf("%d books has been removed.\n", booksRemoved);
 
+    printf("Cleaning all stacks of plates...\n");
+    printf("%d plates has been removed.\n", platesRemoved);
+
+    printf("Cleaning shopping queue...\n");
+    printf("%d robots has been removed.\n", robotsRemoved);
 }
 
 // function to add a robot to a shopping queue
@@ -151,7 +254,7 @@ void SimulateGoForShopping(struct Shopping * shopping)
 }
 
 // function to clean shopping queue before the end of the program
-void CleanShoppingQueue(/*...*/)
+void CleanShoppingQueue()
 {
 
 }
@@ -161,36 +264,57 @@ void CleanShoppingQueue(/*...*/)
 // It generates and consumes events.
 void SimulationLoop(int EventNumbers)
 {
-	// declare and initialize necessary variables
-
+	srand(time(NULL)); // to ensure randomization with each run
+	int eventType;
 	InitStacks();
 	
-	for (int i=0; i<EventNumbers; i++)    
-	{
+	for (int i=0; i<EventNumbers; i++){
 		// generate event type
+		eventType = GenerateEventType();
+		
 		// depending on the generated event type:
-		// event type 0: 
+		// Event type 0: 
+		if (eventType == 0) {
 			// generate RobotPackage 
+			struct RobotPackage* newPackage = GenerateRobotPackage();
 			// Simulate managing RobotPackages (sorting)
-		// event type 1:
-			// generate Package
-			// Simulate classifying Packages (putting to a corresponding stack)
+			SimulateManagingRobotPackages(newPackage);
+		}
+
+		// event type 1: 
+		else if (eventType == 1) {
+			//generate Package
+			struct Package* newPackage = GeneratePackage();
+			// Simulate classifying Packages (pputting to a corresponding stack)
+			SimulateClassifyPackage(newPackage);
+		}
+
 		// event type 2:
+		else if (eventType == 2) {
 			// generate shopping
-			// Simulate go for shopping 
+			struct Shopping* newShopping = GenerateShopping();
+			// Simulate go for shopping
+			SimulateGoForShopping(newShopping);
+		}
+		
 		// UpdateShopping
+		UpdateShoppingQueue();
 	}
+	
 	// CLEANING THE SIMULATION
+	RemoveAllRobotPackages();
+	CleanPackageStacks();
+	CleanShoppingQueue();
 }
+		
 
 int main (int argc, char ** argv)
 {
-	int EventNumbers;
+	int EventNumbers = atoi(argv[1]);
 	printf ("Starting... \n");
 	CheckArguments(argc, argv);
-	
-	// initialize EventNumbers 
 
 	SimulationLoop(EventNumbers);
+	PrintShopping();
 	return 0;
 }
